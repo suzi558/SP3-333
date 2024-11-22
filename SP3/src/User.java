442461ee4993d48d459be03d62a6f7f6264ae62b
suzi558 <suzinmussa04@gmail.com>
2024-11-22 11:33:44 +0100

    færdiggjord User klasse

---
+++ SP3/src/User.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class User {
+    private String username;
+    private String password;
+    private String fullName;
+    public ArrayList<Media>watchedMovies;
+    public ArrayList<Media>savedMovies;
+
+    public User(String username, String password, String fullName) {
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+        this.watchedMovies = new ArrayList<>();
+        this.savedMovies = new ArrayList<>();
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public void watchedMovies (Media movie) {
+        watchedMovies.add(movie);
+    }
+    public void savedMovies (Media movie) {
+        savedMovies.add(movie);
+    }
+}
